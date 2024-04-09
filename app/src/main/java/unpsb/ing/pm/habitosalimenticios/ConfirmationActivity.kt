c5ce7509c5fdba57788ec8c68bb75f33ba3ff005
Leonardo Morales <leo.moralesr23@gmail.com>
2024-04-09 16:35:01 -0300

    navegación hasta la pantalla de confirmación

--- app/src/main/java/unpsb/ing/pm/habitosalimenticios/ConfirmationActivity.kt
+++ app/src/main/java/unpsb/ing/pm/habitosalimenticios/ConfirmationActivity.kt
@@ -1,35 +1,16 @@
 package unpsb.ing.pm.habitosalimenticios
 
-import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import unpsb.ing.pm.habitosalimenticios.databinding.ActivitySurveyBinding
 
-
-class SurveyActivity : AppCompatActivity() {
-
-    private lateinit var binding: ActivitySurveyBinding
-
+class ConfirmationActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        // setContentView(R.layout.activity_survey)
-
-        binding = ActivitySurveyBinding.inflate(layoutInflater)
-        val view = binding.root
-        setContentView(view)
-
-        binding.buttonSave.setOnClickListener {
-            val i = Intent(
-                applicationContext,
-                ConfirmationActivity::class.java
-            )
-            startActivity(i)
-        }
-
+        setContentView(R.layout.activity_confirmation)
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
