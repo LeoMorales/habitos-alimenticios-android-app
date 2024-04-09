e32a17131b92f3f10ac4490986c0b55a0eafabfa
Leonardo Morales <leo.moralesr23@gmail.com>
2024-04-09 17:16:04 -0300

    pasa datos de un spinner a otra pantalla

--- app/src/main/java/unpsb/ing/pm/habitosalimenticios/SurveyActivity.kt
+++ app/src/main/java/unpsb/ing/pm/habitosalimenticios/SurveyActivity.kt
@@ -2,6 +2,10 @@ package unpsb.ing.pm.habitosalimenticios
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,9 +13,10 @@ import androidx.core.view.WindowInsetsCompat
 import unpsb.ing.pm.habitosalimenticios.databinding.ActivitySurveyBinding
 
 
-class SurveyActivity : AppCompatActivity() {
+class SurveyActivity : AppCompatActivity(),  AdapterView.OnItemSelectedListener {
 
     private lateinit var binding: ActivitySurveyBinding
+    private lateinit var selected_portion: String
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,13 +32,42 @@ class SurveyActivity : AppCompatActivity() {
                 applicationContext,
                 ConfirmationActivity::class.java
             )
+
+            Toast.makeText(applicationContext, selected_portion, Toast.LENGTH_SHORT).show()
+
+            i.putExtra("SELECTED_PORTION", selected_portion)
             startActivity(i)
         }
 
+        val spinner = binding.spinnerPorcionValue
+        spinner.onItemSelectedListener = this
+
+        // Create an ArrayAdapter using the string array and a default spinner layout.
+        ArrayAdapter.createFromResource(
+            this,
+            R.array.portions_array,
+            android.R.layout.simple_spinner_item
+        ).also { adapter ->
+            // Specify the layout to use when the list of choices appears.
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+            // Apply the adapter to the spinner.
+            spinner.adapter = adapter
+        }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>, view: View?, pos: Int, id: Long) {
+        // An item is selected. You can retrieve the selected item using
+        // parent.getItemAtPosition(pos).
+
+        selected_portion = parent.adapter.getItem(pos).toString()
+    }
+
+    override fun onNothingSelected(parent: AdapterView<*>) {
+        // Another interface callback.
     }
 }
